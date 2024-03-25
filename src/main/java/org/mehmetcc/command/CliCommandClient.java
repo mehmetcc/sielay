6dfd118f5016a2821a71949073e977e428b94538
mehmetcc <mehmetccm@gmail.com>
2024-03-25 13:35:02 +0300

    Refactor if statement to switch

--- src/main/java/org/mehmetcc/command/CliCommandClient.java
+++ src/main/java/org/mehmetcc/command/CliCommandClient.java
@@ -10,12 +10,11 @@ public class CliCommandClient {
     Printer printer = new Printer(result.isVerbose());
     String content = result.command().getContent();
 
-    if (content.equals(ParserConstants.SHRED)) {
-      new ShredCommand(printer).execute(result);
-    } else if (content.equals(ParserConstants.FILL_DATA)) {
-      new FillDataCommand(printer).execute(result);
-    } else {
-      printer.printError("Error while matching commands.");
+    switch (content) {
+      case ParserConstants.SHRED -> new ShredCommand(printer).execute(result);
+      case ParserConstants.FILL_DATA -> new FillDataCommand(printer).execute(result);
+      case ParserConstants.DUMP_DB -> new DumpDatabaseCommand(printer).execute(result);
+      default -> printer.printError("Error while matching commands.");
     }
   }
 }
